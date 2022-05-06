764fcc1a6ca08ec189173000a069addf4b16930a
gsofte <real.kingstar@outlook.com>
2022-05-05 20:30:14 -0400

    chore: exception handling in rename endpoint

--- src/bud/bud.controller.ts
+++ src/bud/bud.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Get, Put, Query, Req } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Put, Query, Req } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/prisma/prisma.service';
 import type { Request } from 'src/types';
+import { BreedingError, isPrismaError } from 'src/utils/errors';
 import { Gen1BudQueryDto } from './dto/gen1-query.dto';
 import { RenameGen1BudDto } from './dto/rename-bud.dto';
 
 @Controller('buds')
 export class BudController {
+  private readonly logger = new Logger('BudController');
+
   constructor(
     private readonly prismaService: PrismaService,
     private readonly configService: ConfigService,
@@ -35,45 +38,51 @@ export class BudController {
     @Req() req: Request,
     @Body() { budId, name }: RenameGen1BudDto,
   ) {
-    const bud = await this.prismaService.gen1Bud.findFirst({
-      where: {
-        id: budId,
-        minterAddress: req.user,
-      },
-      include: {
-        request: true,
-      },
-    });
-
-    if (!bud) {
-      throw new Error('Bud not found.');
-    }
-
-    const limit = this.configService.get<number>('bud.renameAllowedTime'); // milliseconds
-
-    if (!bud.request.requestedAt || bud.request.requestedAt.getTime() + limit < Date.now()) {
-      throw new Error('Rename too late.');
-    }
-
-    // Verify bud name
-    const isValid = true;
-
-    if (isValid) {
-      const bud = await this.prismaService.gen1Bud.update({
+    try {
+      const bud = await this.prismaService.gen1Bud.findFirst({
         where: {
           id: budId,
+          minterAddress: req.user,
         },
-        data: {
-          name,
+        include: {
+          request: true,
         },
       });
-
-      return {
-        success: true,
-        data: bud,
+  
+      if (!bud) {
+        throw new Error('Bud not found.');
+      }
+  
+      const limit = this.configService.get<number>('bud.renameAllowedTime'); // milliseconds
+  
+      if (!bud.request.requestedAt || bud.request.requestedAt.getTime() + limit < Date.now()) {
+        throw new Error('Rename too late.');
+      }
+  
+      // Verify bud name
+      const isValid = true;
+  
+      if (isValid) {
+        const bud = await this.prismaService.gen1Bud.update({
+          where: {
+            id: budId,
+          },
+          data: {
+            name,
+          },
+        });
+  
+        return {
+          success: true,
+          data: bud,
+        }
+      }
+    } catch (e) {
+      if (isPrismaError(e)) {
+        this.logger.error('finalize', e);
+        throw BreedingError();
       }
     }
-
     return {
       success: false,
       data: null,
