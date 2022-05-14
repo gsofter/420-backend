bb6cb97428cf54983a14f94be714b2dd7e550742
gsofte <real.kingstar@outlook.com>
2022-05-14 11:24:37 -0400

    feat: bud name validity check

--- src/bud/bud.controller.ts
+++ src/bud/bud.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Get, Logger, Put, Query, Req } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 import { PrismaService } from 'src/prisma/prisma.service';
 import type { Request } from 'src/types';
-import { BreedingError, isPrismaError, NotFoundError, UnproceesableEntityError } from 'src/utils/errors';
+import { NotFoundError, UnproceesableEntityError } from 'src/utils/errors';
 import { Gen1BudQueryDto } from './dto/gen1-query.dto';
 import { RenameGen1BudDto } from './dto/rename-bud.dto';
 
@@ -53,7 +54,7 @@ export class BudController {
     }
 
     // Verify bud name
-    const isValid = true;
+    const isValid = await this.checkBudName(name);
 
     if (isValid) {
       const bud = await this.prismaService.gen1Bud.update({
@@ -72,5 +73,26 @@ export class BudController {
     }
 
     throw UnproceesableEntityError('Proposed bud name cannot be accepted. Please try the other one');
+  }
+
+  private async checkBudName(name: string) {
+    try {
+      const apiKey = this.configService.get<string>('metadataApi.key');
+      const network = this.configService.get<string>('network.name');
+
+      const url = `https://420${network === 'rinkeby' ? '-dev' : ''}.looklabs.xyz/${network === 'rinkeby' ? 'dev/' : ''}checkBudName`;
+
+      const response = await axios.get(`${url}?budName=${name}`, {
+        headers: {
+          'X-Api-Key': apiKey
+        }
+      });
+
+      return response.data[0].valid;
+    } catch (e) {
+      this.logger.error('Bud name check API error', e);
+    }
+
+    return false;
   }
 }
