d67db9d79d047720da46b964db9486664065ff49
gsofte <real.kingstar@outlook.com>
2022-06-05 06:54:54 -0400

    feat: add http infos to APM tracing

--- src/utils/logger/tracing.ts
+++ src/utils/logger/tracing.ts
@@ -10,4 +10,20 @@ tracer.init({
   env: process.env.NETWORK
 });
 
+tracer.use("express", {
+  hooks: {
+    request: (span, req) => {
+      if (span && req) {
+        span.addTags({
+          "request-id": req.headers["x-request-id"],
+          "http.user-agent": req.headers["user-agent"],
+          "http.url": req.url,
+          "http.ip": req.headers["x-forwarded-for"],
+          "http.authorization": req.headers["authorization"],
+        });
+      }
+    },
+  },
+});
+
 export default tracer;
