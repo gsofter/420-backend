68134ccc8a6769b625d1771d1d3ac8e716e36786
gsofte <real.kingstar@outlook.com>
2022-08-01 23:08:52 -0400

    chore: ignore logging some URls

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -11,26 +11,38 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   private logger = new Logger('LoggingInterceptor');
+  private ignorelist = ['/health'];
+
+  private shouldIgnore(url: string): boolean {
+    return this.ignorelist.indexOf(url) >= 0;
+  }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
 
     const message = `[${request.method.toUpperCase()}] ${request.url}`;
-    this.logger.log(message, {
-      http: {
-        ip: request.ips.length ? request.ips[0] : request.ip,
-        'user-agent': request.headers['user-agent'],
-        authorization: request.headers['authorization'],
-        method: request.method.toUpperCase(),
-        url: request.url,
-        params: request.params,
-        'request-id': request.headers['x-request-id'],
-      },
-    });
+
+    if (!this.shouldIgnore(request.url)) {
+      this.logger.log(message, {
+        http: {
+          ip: request.ips.length ? request.ips[0] : request.ip,
+          'user-agent': request.headers['user-agent'],
+          authorization: request.headers['authorization'],
+          method: request.method.toUpperCase(),
+          url: request.url,
+          params: request.params,
+          'request-id': request.headers['x-request-id'],
+        },
+      });
+    }
 
     const now = Date.now();
-    return next
-      .handle()
-      .pipe(tap(() => this.logger.log(`Finish(${Date.now() - now} ms): ${message} `)));
+    return next.handle().pipe(
+      tap(() => {
+        if (!this.shouldIgnore(request.url)) {
+          this.logger.log(`Finish(${Date.now() - now} ms): ${message} `);
+        }
+      }),
+    );
   }
 }
