261d890952e2d387641786bb5f50f7c1ef3da992
gsofte <real.kingstar@outlook.com>
2022-08-18 20:15:57 -0400

    chore: add ip test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import type { Request } from 'express';
 
 @Controller('')
 export class AppController {
@@ -6,5 +7,14 @@ export class AppController {
   @Get('health')
   async health() {
     return 'Ok'
+  }
+
+  @Get('ip')
+  async getIp(@Req() req: Request) {
+    return {
+      ip: req.ip,
+      ips: req.ips,
+      host: req.headers['x-forwarded-for']
+    }
   }
 }
